cc1b22d586f8b39290ebccea2f1c944443a7ac48
Hawshemi <rhawshemi@gmail.com>
2023-04-11 02:00:31 -0500

    Added Assignments.
    Added Assignments.

---
+++ 5. Capstone: Retrieving, Processing, and Visualizing Data with Python/2- Gmane/gline.js
@@ -0,0 +1,3 @@
+gline = [ ['Year','unicon.net','cam.ac.uk','mac.com','umich.edu','rsmart.com','mtu.edu','virginia.edu','gmail.com','northwestern.edu','suny.edu'],
+['2005-12',4,3,2,2,2,2,2,2,2,1]
+];
